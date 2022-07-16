1422a77c12ce45176ce4985a8fec560c0d4e385b
serclino <sercl.adam@email.cz>
2022-07-16 22:04:36 +0200

    create NextSteps component

---
+++ src/components/NextSteps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NextSteps = () => {
+  return (
+    <>
+      <div className="component">NextSteps</div>
+    </>
+  );
+};
--- src/components/Singleton.js
+++ src/components/Singleton.js
@@ -1,3 +1,6 @@
+// součástí této komponenty bude možnost
+// vytvořít ÚPLNĚ NOVÝ list (s popUpem)
+
 import React from 'react'
 
 export const Singleton = () => {
