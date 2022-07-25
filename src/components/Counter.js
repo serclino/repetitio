dbcba8aef325669294c4f994daf51d784704116d
serclino <sercl.adam@email.cz>
2022-07-25 10:50:13 +0200

    Counter style 100%

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,17 +6,29 @@ import {
   selectMistakesList,
 } from "../features/listsSlice";
 
+import style from "../styles/components/Counter.module.css";
+
 export const Counter = () => {
   const mainList = useSelector(selectMainList);
   const rolledList = useSelector(selectRolledList);
   const mistakesList = useSelector(selectMistakesList);
 
   return (
-    <>
-      <div className="component">Counter</div>
-      <p>{mainList.length} Remaining</p>
-      <p>{rolledList.length} Done</p>
-      <p>{mistakesList.length} Mistakes</p>
-    </>
+    <section className={style.counter}>
+      <div className={style.box}>
+        <h2>{mainList.length}</h2>
+        <p>Remaining</p>
+      </div>
+      <div className={style.stripe}></div>
+      <div className={style.box}>
+        <h2>{rolledList.length}</h2>
+        <p>Done</p>
+      </div>
+      <div className={style.stripe}></div>
+      <div className={style.box}>
+        <h2>{mistakesList.length}</h2>
+        <p>Mistakes</p>
+      </div>
+    </section>
   );
 };
